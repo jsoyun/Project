a59a91ef3c6cb32675518d5cc37a7398e68457f3
jsoyun <syusmm233@gmail.com>
2024-06-18 00:30:32 +0900

    Fix: align ClothesController

--- project/src/main/java/com/favorite/project/Clothes/ClothesController.java
+++ project/src/main/java/com/favorite/project/Clothes/ClothesController.java
@@ -25,7 +25,6 @@ public class ClothesController {
 
     @PostMapping
     public ResponseEntity<ClothesInsertionResponseDto> addClothes(@RequestBody ClothesAddDto clothesAddDto, HttpServletRequest request) {
-        // TODO: 유효한 옷타입 카테고리인지 검증
         HttpSession session = request.getSession(false);
         if (session != null) {
             User user = (User) session.getAttribute(SessionConst.USER_SESSION_ID);
@@ -50,11 +49,7 @@ public class ClothesController {
 
 
     @GetMapping
-    public ResponseEntity<List<ClothesTotalListResponse>> getClothes(@RequestParam(required = false) SeasonType seasonType,
-                                                                     @RequestParam(required = false) ClothesCategoryEnumType clothesCategoryEnumType,
-                                                                     @RequestParam(required = false) Double minPrice,
-                                                                     @RequestParam(required = false) Double maxPrice
-    ) {
+    public ResponseEntity<List<ClothesTotalListResponse>> getClothes(@RequestParam(required = false) SeasonType seasonType, @RequestParam(required = false) ClothesCategoryEnumType clothesCategoryEnumType, @RequestParam(required = false) Double minPrice, @RequestParam(required = false) Double maxPrice) {
         List<ClothesTotalListResponse> clothesList = clothesService.getClothes(seasonType, clothesCategoryEnumType, minPrice, maxPrice);
         if (clothesList.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
